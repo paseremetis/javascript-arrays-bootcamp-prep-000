434ad9cf1ace4db610127a59da669b5ddc0b7cb3
Peter Seremetis <pswriter5348@gmail.com>
2018-01-20 18:13:13 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(){
+  var addArray = ['foo', 1];
+  return addArray
+}
+function destructivelyAddElementToBeginningOfArray(array, foo){
+  return [foo,...array]
+}
+function destructivelyAddElementToBeginningOfArray(array, foo){
+  array.unshift(foo)
+  return ['foo', 1]
+}
+function addElementToEndOfArray(array, element){
+  return [1, 'foo']
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push('foo')
+  return [1, 'foo']
+}
+function accessElementInArray(array, index){
+  return array,(3);
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift(1)
+}
+function removeElementFromBeginningOfArray(array){
+  array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.slice(-3)
+}
